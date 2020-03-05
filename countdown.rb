555c6221697180d6ae6a274e75005ce87b79c8bc
Ruari Day <ruariday@gmail.com>
2020-03-05 12:31:50 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
-#write your code here
 
-def countdown
+def countdown(seconds)
+  seconds = 10
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds)
+  seconds = 10
+  while seconds > 0 
+  sleep 2
+    puts "#{seconds}SECOND(S)!"
+    seconds -= 1 
+  end 
+  "HAPPY NEW YEAR!"
+end 
+
+
+
+
+
