67a2a8de8a84c04844d7e6cb2cab93a2c02be929
Ruari Day <ruariday@gmail.com>
2020-03-05 12:24:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,4 +8,17 @@ def countdown(seconds)
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(seconds)
+  seconds = 10
+  while seconds > 0 
+  sleep
+    puts "#{seconds}SECOND(S)!"
+    seconds -= 1 
+  end 
+  "HAPPY NEW YEAR!"
+end 
+
+
+
+
+
